36229e1c97ca3c75d86c8f75ba660aba09dbb256
xiuer <xiuer.163.com>
2019-02-22 19:16:36 +0800

    '静态页面'

--- src/main.js
+++ src/main.js
@@ -1,13 +1,15 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
+import App from './App.vue'
 
-Vue.config.productionTip = false
+import router from './router'
+import Header from './components/Header/Header.vue'
 
-/* eslint-disable no-new */
+//全局使用Header
+Vue.component('Header',Header)
+
 new Vue({
-  el: '#app',
-  components: { App },
-  template: '<App/>'
+  el:'#app',
+  components:{App},
+  template:'<App/>',
+  router
 })
